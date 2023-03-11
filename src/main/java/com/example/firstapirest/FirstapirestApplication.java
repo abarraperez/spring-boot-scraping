28e624966f61645e2d2921e8dc58fe91726f0e38
Armando Barra Perez <armando.barra.p@gmail.com>
2023-03-11 01:59:56 -0300

    add first files in spring boot

---
+++ src/main/java/com/example/firstapirest/FirstapirestApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstapirestApplication.class, args);
+	}
+
+}
